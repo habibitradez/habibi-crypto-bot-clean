84afb759f6bac9b2a9d3aef85cbe5ff29b20e4b9
habibitradez <chrisdebest1212@gmail.com>
2025-04-15 14:18:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def real_buy_token(to_addr: str, lamports: int):
         ix = transfer(TransferParams(from_pubkey=keypair.pubkey(), to_pubkey=recipient, lamports=lamports))
         blockhash_resp = solana_client.get_latest_blockhash()
         blockhash = blockhash_resp.value.blockhash
-        msg = Message.new([ix], keypair.pubkey(), blockhash)
+        msg = Message([ix], keypair.pubkey(), blockhash)
         tx = Transaction([keypair], msg)
         serialized_tx = tx.serialize()
         resp = solana_client.send_raw_transaction(serialized_tx)
